3097ed90c2347825ccc8ccf1d1df5c284ff3d147
maanoeln <mneto_004@hotmail.com>
2020-08-21 13:05:49 -0300

    userdb

--- src/database/UserDatabase.ts
+++ src/database/UserDatabase.ts
@@ -1,61 +1,37 @@
-import BaseDB from "./BaseDatabase";
+import { BaseDatabase } from './BaseDatabase';
 
-export default class UserDB extends BaseDB {
-  static tableName = "User_Cookenu"
+export class UserDataBase extends BaseDatabase{
+    private static TABLE_NAME: string = 'User_Cookenu';
 
-  async createUser(
-    id: string,
-    name: string,
-    email: string,
-    password: string
-  ) {
-    await this.getConnection()
-      .insert({id, name, email, password})
-      .into(UserDB.tableName)
+    public async CreateUser (id: string, name: string, email: string, password: string) :Promise<void>{
+        await this.GetConnection()
+        .insert({
+            id,
+            name,
+            email,
+            password
+        }).into(UserDataBase.TABLE_NAME)
+    }
 
-      await this.destroyConnection()
-  }
+    public async GetUserByEmail (email: string) :Promise<any>{
+        const result = await this.GetConnection()
+        .select('*')
+        .from(UserDataBase.TABLE_NAME)
+        .where({
+            email
+        });
 
-  async getUserByMail(email: string) {
-    const result = await this.getConnection()
-      .select('*')
-      .from(UserDB.tableName)
-      .where({email})
-
-      await this.destroyConnection()
-
-      return result[0]
-  }
-
-  async getUserById(id: string) {
-    const result = await this.getConnection()
-      .select('*')
-      .from(UserDB.tableName)
-      .where({id})
-
-      await this.destroyConnection()
-
-      return result[0]
-  }
-
-  async getFeed(id: string): Promise<any> {
-    const result = await this.getConnection()
-      .raw(`SELECT r.id, r.title, r.description, r.created_at as createdAt, uc.followed_id as followedId, u.name as userName
-      FROM ${} r JOIN ${} uc ON r.created_by = uc.followed_id
-      JOIN ${UserDB.tableName} u ON uc.followed_id = u.id
-      WHERE uc.follower_id = "${id}"
-      ORDER BY r.created_at DESC`
-    )
-
-    const newArray = result[0].map((recipe: {createdAt: number}) => {
-      return {
-        ...recipe,
-        createdAt: SVGAnimateMotionElement.unix(recipe.createdAt/100).format("DD/MM/YYYY")
-      }
-    })
-  
-    await this.destroyConnection()
+        return result[0];
+    }
     
-    return newArray
-  }
+    public async GetUserById (id: string) :Promise<any>{
+        const result = await this.GetConnection()
+        .select('*')
+        .from(UserDataBase.TABLE_NAME)
+        .where({
+            id
+        });
+
+        return result[0];
+    }
 }
