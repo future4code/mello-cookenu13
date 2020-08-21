09a388f1e0a1c5cd0d40f711ac2507ec6181e7c5
maanoeln <mneto_004@hotmail.com>
2020-08-21 10:27:19 -0300

    endpoints

---
+++ src/endpoints/getOwnProfile.ts
@@ -0,0 +1,26 @@
+import { Request, Response, request } from "express";
+import Authenticator from "../services/Authenticator"
+import UserDB from "../database/UserDatabase"
+
+async function getOwnProfile (req: Request, res: Response) {
+  try {
+    const retrievedData = Authenticator.getTokenData(req.headers.authorization as string)
+    const userData = await new UserDB().getUserById(retrievedData.id)
+
+    if(!userData) {
+      throw new Error('Faça login primeiro.')
+    }
+
+    res.status(200).send({
+      id: userData.id,
+      name: userData.name,
+      email: userData.email
+    })
+  } catch (err) {
+    res.status(400).send({
+      message: err.sqlMessage || err.message
+    })
+  }
+}
+
+export default getOwnProfile;
